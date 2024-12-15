f215327d106396ee7b15622df246ef5cd8fad108
bilalgill746 <gillbilal932@gmail.com>
2024-12-15 21:22:41 +0500

    Assignments WMA-08

---
+++ Assignment 07/Light Bulb Experiment.js
@@ -0,0 +1,8 @@
+let input=prompt("Enter either 1 (turn ON) or 0 (turn OFF)");
+if(input==1){
+    console.log("The Light is ON")
+}else if(input==0){
+   console.log("The Light is OFF")
+}else{
+    prompt("Invalid Input")
+}
---
+++ Assignment 07/Traffic Light Simulator.js
@@ -0,0 +1,11 @@
+let input=prompt("Enter any of the following color: Red,Yellow,Green");
+if(input=="Red"){
+    console.log("Stop")
+}else if(input=="Yellow"){
+    console.log("Slow Down")
+}else if(input=="Green") 
+{
+    console.log("Go")
+}else{
+    prompt("Invalid Input")
+}
